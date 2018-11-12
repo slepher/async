4a7ce2d1f78050a2f6c05ac442d9ec3645eaec73
slepher <cxyfree@gmail.com>
2018-11-12 15:32:49 +0800

    update async_t

--- src/async_t.erl
+++ src/async_t.erl
@@ -560,10 +560,10 @@ run_info(Info, Offset, State, {?MODULE, IM}) ->
                     async_r_t:run(ARTA, {CallbacksG, CallbacksS}, AccRef, NState);
                 error ->
                     %% TODO : fix this.
-                    monad:return({ok, State}, IM)
+                    monad:return(State, IM)
             end;
         unhandled ->
-            unhandled
+            monad:return(unhandled, IM)
     end.
 
 %%--------------------------------------------------------------------
